a4d6748651148f262ccedcd720a4ff4f55789e4e
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-25 21:04:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
+require 'pry'
 # Add your code here
+class Dog
+
+  @@all = []
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    binding.pry
+    @@all.each do |name|
+      puts "#{name}"
+    end
+  end
+
+end
